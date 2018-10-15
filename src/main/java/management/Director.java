771679b631a153cebbd360ad5407108b1fe81f5d
self-unit <self_unit@hotmail.com>
2018-10-15 18:59:15 +0100

    Director all tests passing

--- src/main/java/management/Director.java
+++ src/main/java/management/Director.java
@@ -1,4 +1,15 @@
 package management;
 
-public class Director {
+public class Director extends Manager {
+
+    private double budget;
+
+    public Director(String name, int niNumber, double salary, String deptName, double budget) {
+        super(name, niNumber, salary, deptName);
+        this.budget = budget;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
 }
--- src/test/java/DirectorTest.java
+++ src/test/java/DirectorTest.java
@@ -1,2 +1,51 @@
+import management.Director;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DirectorTest {
+
+    Director director;
+
+    @Before
+    public void before() {
+        director = new Director("Phillip Morris", 9275023, 120000.00, "Board of Directors", 2400000.00);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Phillip Morris", director.getName());
+    }
+
+    @Test
+    public void hasNiNumber() {
+        assertEquals(9275023, director.getNiNumber());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(120000.00, director.getSalary(), 0.01);
+    }
+
+    @Test
+    public void hasDeptName() {
+        assertEquals("Board of Directors", director.getDeptName());
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        director.raiseSalary(12000.00);
+        assertEquals(132000.00, director.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canPayBonus() {
+        assertEquals(1200.00, director.payBonus(), 0.01);
+    }
+
+    @Test
+    public void getBudget() {
+        assertEquals(2400000.00, director.getBudget(), 0.01);
+    }
 }
